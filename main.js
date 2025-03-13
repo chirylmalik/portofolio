476b92f9bbc0ef4ed498964d8b106ce2fb380956
Chiryl Malik <chirylmalik0@gmail.com>
2025-03-13 13:40:43 +0700

    add contact

--- main.js
+++ main.js
@@ -45,3 +45,23 @@ function educationItemHover(element, isHovering) {
         p.fadeOut(200)
     }
 }
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if (!$(email).val()) {
+        alert("Email is required")
+    } else if (!$(subject).val) {
+        alert("Subject is required")
+    } else if (!$(message).val) {
+        alert("Message is required")
+    } else {
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+        alert("Form Submitted")
+    }
+}
